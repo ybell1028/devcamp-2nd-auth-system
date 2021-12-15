f3de46d1ec50858b40feacbce973e33cbf25e86f
황종훈 <ybell1028@gmail.com>
2021-12-16 01:24:09 +0900

    Feat : Eureka Dicovery Server 추가

--- auth-server/src/main/java/com/smilegate/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/smilegate/authserver/AuthServerApplication.java
@@ -1,13 +1,22 @@
 package com.smilegate.authserver;
 
+import lombok.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class AuthServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
     }
 
+    @GetMapping("/info")
+    public String info() {
+        return "Auth 서비스의 기본 동작 Port:";
+    }
 }
